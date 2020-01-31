82509c6bb0d3c6d0967df641bba04afd9d6ed554
eightball <50410328+Aisaac23@users.noreply.github.com>
2020-01-31 13:05:40 -0600

    Add files via upload

--- dca.c
+++ dca.c
@@ -11,16 +11,41 @@ As an input you can provide the name of a file; or an empty string "" so all the
 #include <time.h>
 #include <stdlib.h>
 #include <locale.h>
+#define ARRAY_SIZE 10000
 
 void deviceCapabilitiesAnalizer( char *stream );
 long double factorial(long double n);
 int compareints (const void * a, const void * b);
 
+
+clock_t tOneScond, tCounter;
+int array[ARRAY_SIZE];
+unsigned int times = 0;
+
+void intLimits( FILE *stream );
+void charLimits( FILE *stream );
+void floatLimits( FILE *stream );
+void mantisaAndExponentLimits(FILE *stream);
+void epsilonLimits(FILE *stream);
+void swapsPerSecond(FILE *stream);
+void comparisonsPerSecond(FILE *stream);
+void memoryAllocationsPerSecond(FILE *stream);
+void linearSearchesPerSecond(FILE *Stream);
+void binarySearchesPerSecond( FILE *Stream );
+void localeSettings(FILE *stream);
+
 int main(int argc, char *argv[])
 {
+	FILE * tempFile;
 	if( argc < 2 )
-		argv[1] = NULL;
-	deviceCapabilitiesAnalizer(argv[1]);
+	{
+		deviceCapabilitiesAnalizer(NULL);
+	}
+	else if( (tempFile = fopen(argv[1], "a") ) == NULL )
+		deviceCapabilitiesAnalizer(argv[1]);
+	else
+		printf("Any of the necessary arguments are missing or with a wrong format");
+	
 	return EXIT_SUCCESS;
 }
 
@@ -33,13 +58,44 @@ void deviceCapabilitiesAnalizer(char *fileName)
 		stream = fopen(fileName, "a");
 
 	fprintf(stream, "*******Integer and character types*******\n\n");
+	intLimits( stream );
+	charLimits( stream );
 	
+	fprintf(stream, "\n*******Floating point types*******\n\n");
+	floatLimits( stream );
+	mantisaAndExponentLimits(stream);
+	epsilonLimits(stream);
+
+	fprintf(stream, "\n*******Processing*******\n\n");
+	//Clock ticks are units of time of a constant but system-specific length, as those returned by function clock.
+	fprintf(stream, "Clocks per second:%lu\n\n", CLOCKS_PER_SEC);
+
+	for(int a = 0; a<ARRAY_SIZE; a++)
+		array[a] = a;
+
+	swapsPerSecond(stream);
+	comparisonsPerSecond(stream);
+	memoryAllocationsPerSecond(stream);
+	linearSearchesPerSecond(stream);
+	binarySearchesPerSecond( stream );
+	localeSettings(stream);
+
+	if( fileName != NULL )
+		fclose(stream);
+	
+}
+
+int compareints (const void *a, const void *b)
+{
+  return ( *(int*)a - *(int*)b );
+}
+
+void intLimits( FILE *stream )
+{
 	fprintf(stream, "int (storage size):%-lu\n", sizeof(int) );
-	fprintf(stream, "char (storage size):%-lu\n", sizeof(char) );
 	fprintf(stream, "short (storage size):%-lu\n", sizeof(short) );
 	fprintf(stream, "long int (storage size):%-lu\n", sizeof(long int) );
 	fprintf(stream, "long long int (storage size):%-lu\n", sizeof(long long int) );
-	fprintf(stream, "Multybyte char for any locale (bytes):%-d\n\n", MB_LEN_MAX);
 	
 	fprintf(stream, "int (max):%d\n", INT_MAX);
 	fprintf(stream, "int (min):%d\n\n", INT_MIN);
@@ -47,22 +103,32 @@ void deviceCapabilitiesAnalizer(char *fileName)
 	fprintf(stream, "long (max):%ld\n", (long) LONG_MAX);
 	fprintf(stream, "long (min):%ld\n\n", (long) LONG_MIN);
 
-	fprintf(stream, "char (max):%d\n", CHAR_MAX);
-	fprintf(stream, "char (min):%d\n", CHAR_MIN);
-	fprintf(stream, "char (bits):%d\n\n", CHAR_BIT);
-
 	fprintf(stream, "short (max):%d\n", SHRT_MAX);
 	fprintf(stream, "short (min):%d\n\n", SHRT_MIN);
-
-	fprintf(stream, "signed char (max):%d\n", SCHAR_MAX);
-	fprintf(stream, "signed char (min):%d\n\n", SCHAR_MIN);
 
 	fprintf(stream, "unsigned int (max):%u\n", (unsigned int) UINT_MAX);
 	fprintf(stream, "unsigned long (max):%lu\n", (unsigned long) ULONG_MAX);
 	fprintf(stream, "unsigned char (max):%d\n", UCHAR_MAX);
 	fprintf(stream, "unsigned short (max):%d\n\n", (unsigned short) USHRT_MAX);
-	
-	fprintf(stream, "\n*******Floating point types*******\n\n");
+
+}
+
+void charLimits( FILE *stream )
+{
+	fprintf(stream, "char (storage size):%-lu\n", sizeof(char) );
+	fprintf(stream, "Multybyte char for any locale (bytes):%-d\n\n", MB_LEN_MAX);
+
+	fprintf(stream, "char (max):%d\n", CHAR_MAX);
+	fprintf(stream, "char (min):%d\n", CHAR_MIN);
+	fprintf(stream, "char (bits):%d\n\n", CHAR_BIT);
+
+	fprintf(stream, "signed char (max):%d\n", SCHAR_MAX);
+	fprintf(stream, "signed char (min):%d\n\n", SCHAR_MIN);
+}
+
+void floatLimits( FILE *stream )
+{
+
 	fprintf(stream, "float (storage size):%lu\n", sizeof(float) );
 	fprintf(stream, "double (storage size):%lu\n", sizeof(double) );
 	fprintf(stream, "long double (storage size):%lu\n\n", sizeof(long double));
@@ -80,7 +146,11 @@ void deviceCapabilitiesAnalizer(char *fileName)
 	fprintf(stream, "Precision value (float):%d\n", FLT_DIG );
 	fprintf(stream, "Precision value (double):%d\n", DBL_DIG);
 	fprintf(stream, "Precision value (long double):%d\n\n", LDBL_DIG );
-	
+
+}
+
+void mantisaAndExponentLimits(FILE *stream)
+{
 	fprintf(stream, "Mantisa bits / significand (float):%d\n", FLT_MANT_DIG);
 	fprintf(stream, "Mantisa bits / significand (double):%d\n", DBL_MANT_DIG);
 	fprintf(stream, "Mantisa bits / significand (long double):%d\n\n", LDBL_MANT_DIG);
@@ -92,23 +162,19 @@ void deviceCapabilitiesAnalizer(char *fileName)
 	fprintf(stream, "Maximum int value for the exponent (float):%d\n", FLT_MAX_EXP);
 	fprintf(stream, "Maximum int value for the exponent (double):%d\n", DBL_MAX_EXP);
 	fprintf(stream, "Maximum int value for the exponent (long double):%d\n\n", LDBL_MAX_EXP );
+}
+
+void epsilonLimits(FILE *stream)
+{
 
 	fprintf(stream, "Difference between 1 and the least value greater than 1 that is representable. (float):%g\n", FLT_EPSILON);
 	fprintf(stream, "Difference between 1 and the least value greater than 1 that is representable. (double):%g\n", DBL_EPSILON);
 	fprintf(stream, "Difference between 1 and the least value greater than 1 that is representable. (long double):%Lg\n\n", LDBL_EPSILON );
 
-	fprintf(stream, "\n*******Processing*******\n\n");
-	//Clock ticks are units of time of a constant but system-specific length, as those returned by function clock.
-	fprintf(stream, "Clocks per second:%lu\n\n", CLOCKS_PER_SEC);
+}
 
-	const unsigned int ARRAY_SIZE = 10000;
-	clock_t tOneScond, tCounter;
-  	int array[ARRAY_SIZE];
-	unsigned int times = 0;
-
-	for(int a = 0; a<ARRAY_SIZE; a++)
-		array[a] = a;
-
+void swapsPerSecond(FILE *stream)
+{
 	times = 0;
 	tCounter = clock();
 	tOneScond = clock()+CLOCKS_PER_SEC;
@@ -121,7 +187,10 @@ void deviceCapabilitiesAnalizer(char *fileName)
 		tCounter = clock();
 	}
 	fprintf(stream, "Swaps per second:%u\n", times);
-	
+}
+
+void comparisonsPerSecond(FILE *stream)
+{
 	times = 0;
 	tCounter = clock();
 	tOneScond = clock()+CLOCKS_PER_SEC;
@@ -131,7 +200,10 @@ void deviceCapabilitiesAnalizer(char *fileName)
 		tCounter = clock();	
 	}
 	fprintf(stream, "Comparisons per second:%u\n", times);
+}
 
+void memoryAllocationsPerSecond(FILE *stream)
+{
 	times = 0;
 	tCounter = clock();
 	tOneScond = clock()+CLOCKS_PER_SEC;
@@ -143,7 +215,10 @@ void deviceCapabilitiesAnalizer(char *fileName)
 		tCounter = clock();
 	}
 	fprintf(stream, "Memory allocations per second:%u\n", times);
+}
 
+void linearSearchesPerSecond(FILE *stream)
+{
 	int key;
 	times = 0;
 	tCounter = clock();
@@ -162,9 +237,11 @@ void deviceCapabilitiesAnalizer(char *fileName)
 		tCounter = clock();
 	}
 	fprintf(stream, "Linear searches in a %u length array:%u\n", ARRAY_SIZE, times);
+}
 
-	int * pItem;
-
+void binarySearchesPerSecond( FILE *stream )
+{
+	int * pItem, key;
 	times = 0;
 	tCounter = clock();
 	tOneScond = clock()+CLOCKS_PER_SEC;
@@ -177,21 +254,14 @@ void deviceCapabilitiesAnalizer(char *fileName)
 		tCounter = clock();
 	}
 	fprintf(stream, "Binary searches in a %u length array:%u\n", ARRAY_SIZE, times);
-	
+}
+
+void localeSettings(FILE *stream)
+{
 	char* locale;
 	locale = setlocale(LC_ALL, NULL);
 	fprintf(stream, "\nCurrent set locale: %s\n", locale);
 	locale = setlocale(LC_ALL,"");
 	fprintf(stream, "Current environment's locale: %s\n", locale);
 	locale = setlocale(LC_ALL, NULL);
-
-	if( fileName != NULL )
-		fclose(stream);
-	
 }
-
-int compareints (const void *a, const void *b)
-{
-  return ( *(int*)a - *(int*)b );
-}
-
